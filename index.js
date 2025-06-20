d9012a0092a35631893040406b3c38d007893e9a
Ammar Ismail Khocan <ammarkhocan02@gmail.com>
2025-06-20 21:23:19 +0700

    add new data

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const contact = {
-  id: 1,
-  fullName: "Ammar Ismail Khocan",
-  email: "ammarexample@gmail.com",
-  phone: "+62 812002333",
-  address: {
-    streetAddress: "Jl. Nusantara No.12",
-    city: "Bandar Lampung",
-    postalCode: 12344,
+const contact = [
+  {
+    id: 1,
+    fullName: "Ammar Ismail Khocan",
+    email: "ammarexample@gmail.com",
+    phone: "+62 812002333",
+    address: {
+      streetAddress: "Jl. Nusantara No.12",
+      city: "Bandar Lampung",
+      postalCode: 12344,
+    },
+    isFavorite: true,
   },
-  isFavorite: true,
-};
-
+  {
+    id: 2,
+    fullName: "Jason",
+    email: "jason1example@gmail.com",
+    phone: "+62 822092287",
+    address: {
+      streetAddress: "Jl. Asoka No.90",
+      city: "Medan",
+      postalCode: 22023,
+    },
+    isFavorite: false,
+  },
+];
 console.log(contact);
