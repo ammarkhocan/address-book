7dfb86953550546d80f534e8f2a6cb72ae5523f1
Ammar Ismail Khocan <ammarkhocan02@gmail.com>
2025-06-20 21:12:43 +0700

    initial project contact data

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const contact = {
+  fullName: "Ammar Ismail Khocan",
+  email: "ammarexample@gmail.com",
+  phone: "+62 812002333",
+  address: {
+    streetAddress: "Jl. Nusantara No.12",
+    city: "Bandar Lampung",
+    postalCode: 12344,
+  },
+};
